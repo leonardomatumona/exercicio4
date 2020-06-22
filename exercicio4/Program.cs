3cb7d4af78537a5a472a634eefad085e464d508d
leonardomatumona <leonardomatumona@gmail.com>
2020-06-22 17:27:32 -0300

    Add project files.

---
+++ exercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double senha1;
+            double senha2;
+            double senhacorreta;
+
+
+            Console.WriteLine("digite a senha:");
+            senha1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Senha Invalida");
+            senhacorreta = double.Parse(Console.ReadLine());
+            Console.WriteLine("Acesso Permitido");
+
+
+
+
+        }
+    }
+}
